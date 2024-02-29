15ee2e22e1ade831339e58f504471cab92896207
Jatimpark <133945139+Jatimpark@users.noreply.github.com>
2024-02-29 23:44:49 +0700

    Add files via upload

--- lanjut.sh
+++ lanjut.sh
@@ -566,7 +566,7 @@ print_success "files Quota Service"
 function ssh_slow(){
 clear
 print_install "Memasang modul SlowDNS Server"
-wget -q -O /tmp/nameserver "${REPO}Fls/nameserver" >/dev/null 2>&1
+wget -q -O /tmp/nameserver "${REPO}slowdns/nameserver" >/dev/null 2>&1
 chmod +x /tmp/nameserver
 bash /tmp/nameserver | tee /root/install.log
 print_success "SlowDNS"
